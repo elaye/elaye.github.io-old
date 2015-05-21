9fb475df7aa0c7aea66edaddc8daaf570f26b571
elaye <genard.elie@orange.fr>
2015-05-21 17:25:27 +0100

    Use shader for face displacement

--- js/sphere.js
+++ js/sphere.js
@@ -1,5 +1,5 @@
 var ExtrudedIcosphere = function(){
-	var ico = new THREE.IcosahedronGeometry(2, 1);
+	var ico = new THREE.IcosahedronGeometry(2, 3);
 	var extIco = [];
 
 	ico.faces.forEach(function(f, i){
@@ -22,11 +22,38 @@ var ExtrudedIcosphere = function(){
 	return extIco;
 }
 
-function displaceFace(geometry, faceId){
-	var face = geometry.faces[faceId];
-	geometry.vertices[face.a].x += 4.0;
-	geometry.vertices[face.b].x += 4.0;
-	geometry.vertices[face.c].x += 4.0;
+function displaceFaces(faces){
+	extIco.forEach(function(geometry){
+		geometry.mouseOver = false;
+	});
+
+	faces.forEach(function(intObject){
+		var baseGeometry = intObject.object.geometry;
+		var geometry = extIco[baseGeometry.faceId];
+		geometry.mouseOver = true;
+		if(geometry.displaced === false){
+			geometry.displaced = true;
+			var faceNormal = baseGeometry.faces[0].normal;
+			displaceFace(geometry.vertices, faceNormal);
+			geometry.verticesNeedUpdate = true;			
+		}
+	});
+
+	extIco.forEach(function(geometry, i){
+		if(geometry.mouseOver === false && geometry.displaced === true){
+			geometry.vertices.forEach(function(v, j){
+					v.copy(baseExtIco[i].vertices[j]);
+					geometry.displaced = false;
+			});
+			geometry.verticesNeedUpdate = true;
+		}
+	});
+}
+
+function displaceFace(face, dir){
+	face.forEach(function(v){
+		v.add(dir);
+	});
 }
 
 var raycaster = new THREE.Raycaster();
@@ -45,19 +72,82 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 100 );
 
 var renderer = new THREE.WebGLRenderer();
+// renderer.setFaceCulling("front", "ccw");
 // renderer.antialias = true;
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor( 0xffffff, 1);
 document.body.appendChild( renderer.domElement );
 
 var baseExtIco = new ExtrudedIcosphere();
 var extIco = new ExtrudedIcosphere();
 
-var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-material.wireframe = true;
+// var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+// material.wireframe = true;
 
+// Material
+// var pinkMat = new THREE.MeshPhongMaterial({
+  // color      :  new THREE.Color("rgb(226,35,213)"),
+  // emissive   :  new THREE.Color("rgb(255,128,64)"),
+  // specular   :  new THREE.Color("rgb(255,155,255)"),
+  // shininess  :  10,
+  // shading    :  THREE.FlatShading,
+  // transparent: 1,
+  // opacity    : 1
+// });
+
+// var L1 = new THREE.PointLight( 0xffffff, 1);
+// L1.position.z = 100;
+// L1.position.y = 100;
+// L1.position.x = 100;
+// scene.add(L1);
+
+// var L2 = new THREE.PointLight( 0xffffff, 0.8);
+// L2.position.z = 200;
+// L2.position.y = 50;
+// L2.position.x = -100;
+// scene.add(L2);
+
+var uniforms = {
+	time: { type: "f", value: 1.0 },
+	resolution: { type: "v2", value: new THREE.Vector2() },
+	intPos: {type: "v3", value: new THREE.Vector3() },
+	lightPos: {type: "v3", value: new THREE.Vector3(2.0, 1.0, 1.0)}
+
+};
+
+// var vertexShader = "			void main()	{
+
+// 				gl_Position = vec4( position, 1.0 );
+
+// 			}";
+
+// var fragmentShader = "			uniform vec2 resolution;
+// 			uniform float time;
+
+// 			void main()	{
+
+// 				// vec2 p = -1.0 + 2.0 * gl_FragCoord.xy / resolution.xy;
+// 				gl_FragColor=vec4(1.0, 0.0, 0.0, 1.0);
+
+// 			}";
+var material = new THREE.ShaderMaterial( {
+
+	uniforms: uniforms,
+	vertexShader: document.getElementById( 'vertexShader' ).textContent,
+	fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+	// vertexShader: vertexShader,
+	// fragmentShader: fragmentShader
+
+} );
+
+material.side = THREE.DoubleSide;
+// material.wireframe = true;
 var meshes = [];
 extIco.forEach(function(g, i){
+	// var mesh = new THREE.Mesh(g, material);
+	// var mesh = new THREE.Mesh(g, pinkMat);
 	var mesh = new THREE.Mesh(g, material);
+	// mesh.doubleSided = true;
 	var baseMesh = new THREE.Mesh(baseExtIco[i], material);
 	meshes.push(baseMesh);
 	scene.add(mesh);
@@ -67,6 +157,8 @@ camera.position.z = 5;
 
 var render = function () {
 	requestAnimationFrame( render );
+	// renderer.setFaceCulling(false);
+
 
 	// update the picking ray with the camera and mouse position	
 	raycaster.setFromCamera( mouse, camera );	
@@ -74,37 +166,13 @@ var render = function () {
 	// calculate objects intersecting the picking ray
 	// var intersects = raycaster.intersectObjects(scene.children);
 	var intersects = raycaster.intersectObjects(meshes);
-
-	material.color.set(0xffffff);
-
-	extIco.forEach(function(geometry){
-		geometry.mouseOver = false;
-	});
-
-	intersects.forEach(function(intObject){
-		var baseGeometry = intObject.object.geometry;
-		var geometry = extIco[baseGeometry.faceId];
-		geometry.mouseOver = true;
-		// console.log(geometry.mouseOver);
-		if(geometry.displaced === false){
-			geometry.displaced = true;
-			var faceNormal = baseGeometry.faces[0].normal;
-			geometry.vertices.forEach(function(v, i){
-				v.add(faceNormal);
-			});
-			geometry.verticesNeedUpdate = true;			
-		}
-	});
-
-	extIco.forEach(function(geometry, i){
-		if(geometry.mouseOver === false && geometry.displaced === true){
-			geometry.vertices.forEach(function(v, j){
-					v.copy(baseExtIco[i].vertices[j]);
-					geometry.displaced = false;
-			});
-			geometry.verticesNeedUpdate = true;
-		}
-	});
+	if(intersects.length > 0){
+		uniforms.intPos.value = intersects[0].point;
+	}
+	else{
+		uniforms.intPos.value = new THREE.Vector3();
+	}
+	// displaceFaces(intersects);
 
 	// cube.rotation.x += 0.1;
 	// meshes[0].rotation.y += 0.01;
