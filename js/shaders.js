d786798da015134ea3350458a34d5cc0cb031139
elaye <genard.elie@orange.fr>
2015-05-28 09:18:57 +0100

    Clean and refactor shader code

--- js/shaders.js
+++ js/shaders.js
@@ -33,7 +33,6 @@ var shaders = {
 			"attribute vec3 faceNormal;",
 
 			"varying vec4 varyingDiffuse;",
-			"varying vec2 varyingUv;",
 
 			"float easeOut(float x, float a){",
 				"float t = 1.0 - exp(-x * a);",
@@ -41,88 +40,75 @@ var shaders = {
 			"}",
 
 			"void displace(float d, inout vec3 p){",
-				"float rec = easeOut(reconstructCnt, rewindSpeed);",
-				"float normalDisAmp = bMouseOver * rec * normalAmp * gauss(normalDev, d);",
+				"float rewind = easeOut(reconstructCnt, rewindSpeed);",
+				// Normal displacement
+				"float normalDisAmp = bMouseOver * rewind * normalAmp * gauss(normalDev, d);",
 				"vec3 normalDis = normalDisAmp * faceNormal;",
 
-				"vec3 noiseDir = normalize(p - center);",
-				"vec3 noise = noiseAmp * snoise(vec4(noisePositionScale * p.xy, noiseTimeScale * mouseOutCnt, noiseTimeScale * rec)) * noiseDir;",
-
-				// "float lateralDisAmp = bMouseOver * rec * lateralAmp * exp(-d * d * 5.0);",
-				// "float lateralDisAmp = bMouseOver * rec * lateralAmp *gauss(lateralDev, d);",
-				// "float lateralDisAmp = bMouseOver * rec * lateralAmp * exp(-d * lateralDev);",
-				"float lateralDisAmp = bMouseOver * rec * lateralAmp * exp( (normalDisAmp - 1.0) * lateralDev);",
+				// Lateral displacement
+				"float lateralDisAmp = bMouseOver * rewind * lateralAmp * exp( (normalDisAmp - 1.0) * lateralDev);",
 				"vec3 lateralDir = cross(-intPos, cross(intPos, faceCenter));",
 				"vec3 lateralDis = lateralDisAmp * lateralDir;",
+
+				// Noise
+				"vec3 noiseDir = normalize(p - center);",
+				"vec3 noise = noiseAmp * snoise(vec4(noisePositionScale * p.xy, noiseTimeScale * mouseOutCnt, noiseTimeScale * rewind)) * noiseDir;",
 
 				"p += normalDis + lateralDis + noise;",
 			"}",
 
+			"vec4 randomQuaternion(vec4 v){",
+				"float u = snoise(v);",
+				"u = (u + 1.0) / 2.0;",
+				"return randomQuaternion(u, u, u);",
+			"}",
+
 			"void randomRotate(float d, inout vec3 p){",
 				"p -= faceCenter;",
-				"vec4 q;",
 				"float amp = bMouseOver * 50.0 * exp(-d * d * 4.0);",
 
-				"float u0 = snoise(vec4(0.0, faceCenter.x, faceCenter.y, faceCenter.z));",
-				"u0 = (u0 + 1.0) / 2.0;",
-				"vec4 q0 = randomQuaternion(u0, u0, u0);",
-				// "inverseQuaternion(q0);",
-				"float u1 = snoise(vec4(amp, faceCenter.x, faceCenter.y, faceCenter.z));",
-				"float u2 = snoise(vec4(amp, faceCenter.x, faceCenter.y, faceCenter.z));",
-				"float u3 = snoise(vec4(amp, faceCenter.x, faceCenter.y, faceCenter.z));",
-				"u1 = (u1 + 1.0) / 2.0;",
-				"u2 = (u2 + 1.0) / 2.0;",
-				"u3 = (u3 + 1.0) / 2.0;",
-				"q = randomQuaternion(u1, u2, u3);",
-				// "q = multQuat(randomQuaternion(u1, u2, u3), inverseQuaternion(q0));",
-				// "q = quaternionMult(q0, inverseQuaternion(q0));",
-				// "q = multQuat(q0, inverseQuaternion(q0));",
-				// "q = multQuat(q0, inverseQuaternion(q0));",
-				// "rotate(p, q);",
-				// "p = rotate_vector(q0, p);",
+				"vec4 q0 = randomQuaternion(vec4(0.0, faceCenter));",
+				"vec4 q = randomQuaternion(vec4(amp, faceCenter));",
+
 				"p = rotate_vector(inverseQuaternion(q0), p);",
 				"p = rotate_vector(q, p);",
-				// "p = rotate_vector(inverseQuaternion(q0), p);",
-				// "p = rotate_vector(multQuat(q, inverseQuaternion(q0)), p);",
+
 				"p += faceCenter;",
 			"}",
 
+			"vec4 diffuseLight(vec3 color, vec3 lightPos){",
+				"float cos = dot(normal,normalize(lightPos));",
+				"cos = clamp(cos, 0.0, 1.0);",
+				"return cos * vec4(color, 1.0);",
+			"}",
+
 			"void main(){",
-				"vec3 newPosition = position;",
 				"float d = distance(normal, intPos - center);",
+
+				"vec3 newPosition = position;",
 				"randomRotate(d, newPosition);",
 				"displace(d, newPosition);",
+				
 				"gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);",
 
-				"varyingDiffuse = vec4(diffuseColor1, 1.0) * clamp(dot(normal, normalize(lightPos1)), 0.0, 1.0);",
-				"varyingDiffuse += vec4(diffuseColor2, 1.0) * clamp(dot(normal, normalize(lightPos2)), 0.0, 1.0);",
-				"varyingUv = uv;",
+				"varyingDiffuse = diffuseLight(diffuseColor1, lightPos1);",
+				"varyingDiffuse += diffuseLight(diffuseColor2, lightPos2);",
 			"}"
 	].join("\n"),
 
 	"fragment": [
-			// THREE.ShaderChunk["common"],
 			"uniform vec2 resolution;",
 			"uniform float time;",
-
-			"uniform sampler2D tex;",
 
 			"uniform vec3 ambientColor;",
 
 			"varying vec4 varyingDiffuse;",
-			"varying vec2 varyingUv;",
 
 			"void main(){",
-				"// vec4 texColor = vec4(inputToLinear(texture2D(tex, varyingUv).xyz), 1.0);",
-				// "vec4 texColor = texture2D(tex, varyingUv);",
-
 				"vec4 ambient = vec4(ambientColor, 1.0);",
-				// "vec4 diffuse = vec4(varyingDiffuse.xyz * texColor.xyz, 1.0);",
 				"vec4 diffuse = varyingDiffuse;",
-				"vec4 color = ambient + diffuse;",
 
-				"// gl_FragColor = color * vec4(vec3(1.0) + 0.5 * texColor.xyz, 1.0);",
-				// "color.w = 0.9;",
+				"vec4 color = ambient + diffuse;",
 				"gl_FragColor = color;",
 			"}"
 	].join("\n")
